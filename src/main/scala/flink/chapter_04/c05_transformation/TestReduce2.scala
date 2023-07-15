41ec04ba8e8b522eaec6c02148443a45674f5924
steveli1840 <steveli1840@gmail.com>
2023-07-15 23:23:01 +0800

    Test reduce2

---
+++ src/main/scala/flink/chapter_04/c05_transformation/TestReduce2.scala
@@ -0,0 +1,24 @@
+package flink.chapter_04.c05_transformation
+
+import org.apache.flink.api.common.functions.ReduceFunction
+import org.apache.flink.api.scala.createTypeInformation
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+
+object TestReduce2 {
+
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    val ds1 = env.fromElements(Score("Zhang", "English", 98), Score("Li", "English", 88), Score("Zhang", "Math", 78), Score("Li", "Match", 68))
+    ds1.keyBy(_.name).reduce(new MyReduceFunc).print()
+    env.execute("Test Reduce2")
+  }
+
+}
+
+case class Score(name: String, course: String, score: Int)
+
+class MyReduceFunc() extends ReduceFunction[Score] {
+  override def reduce(value1: Score, value2: Score): Score = {
+    Score(value1.name, "Sum", value1.score + value2.score)
+  }
+}
