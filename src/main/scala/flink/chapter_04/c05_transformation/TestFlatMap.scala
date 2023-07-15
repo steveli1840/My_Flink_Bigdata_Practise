949c931d17c11c1cd02e96c88e2887a631f3c232
steveli1840 <steveli1840@gmail.com>
2023-07-15 22:54:59 +0800

    test flat map

---
+++ src/main/scala/flink/chapter_04/c05_transformation/TestFlatMap.scala
@@ -0,0 +1,14 @@
+package flink.chapter_04.c05_transformation
+
+import org.apache.flink.api.scala.createTypeInformation
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+
+object TestFlatMap {
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    val ds1 = env.fromCollection(List("hadoop hello scala", "flink hello"))
+    val ds2 = ds1.flatMap(_.split(" "))
+    ds2.print()
+    env.execute("TestFlatMap")
+  }
+}
