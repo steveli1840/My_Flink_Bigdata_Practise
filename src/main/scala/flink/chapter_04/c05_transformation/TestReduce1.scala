76ff726118a0441acc93a8099484a9457ee63864
steveli1840 <steveli1840@gmail.com>
2023-07-15 23:13:38 +0800

    test reduce 1

---
+++ src/main/scala/flink/chapter_04/c05_transformation/TestReduce1.scala
@@ -0,0 +1,21 @@
+package flink.chapter_04.c05_transformation
+
+import org.apache.flink.api.common.functions.ReduceFunction
+import org.apache.flink.api.scala.createTypeInformation
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+
+object TestReduce1 {
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    val ds1 = env.fromCollection(List(("zhangsan", 98), ("lisi", 88), ("wangwu", 78), ("zhangsan", 68), ("lisi", 58), ("wangwu", 48)))
+    val kds = ds1.keyBy(_._1)
+    val reducedDS = kds.reduce(new ReduceFunction[(String, Int)] {
+      override def reduce(value1: (String, Int), value2: (String, Int)): (String, Int) = {
+        (value1._1, value1._2 + value2._2)
+      }
+    })
+    reducedDS.print()
+    env.execute("TestReduce1")
+  }
+
+}
