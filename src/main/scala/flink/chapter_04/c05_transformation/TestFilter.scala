d5f7baa914c0aa388415746910887f220200c742
steveli1840 <steveli1840@gmail.com>
2023-07-15 23:01:43 +0800

    Test filter

---
+++ src/main/scala/flink/chapter_04/c05_transformation/TestFilter.scala
@@ -0,0 +1,15 @@
+package flink.chapter_04.c05_transformation
+
+import org.apache.flink.api.scala.createTypeInformation
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+
+object TestFilter {
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    val ds1 = env.fromCollection(List(1, 2, 3, 4, 5, 6))
+    val ds2 = ds1.filter(_ > 3)
+    ds2.print()
+    env.execute("TestFilter")
+  }
+
+}
