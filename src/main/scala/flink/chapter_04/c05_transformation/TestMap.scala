ba91db35f82b0fa90e6db204137b09742a156860
steveli1840 <steveli1840@gmail.com>
2023-07-15 22:52:12 +0800

    TestMap

---
+++ src/main/scala/flink/chapter_04/c05_transformation/TestMap.scala
@@ -0,0 +1,14 @@
+package flink.chapter_04.c05_transformation
+
+import org.apache.flink.streaming.api.scala._
+
+object TestMap {
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    val ds1 = env.fromCollection(List(1, 2, 3, 4, 5))
+    val ds2 = ds1.map(x => x + 1)
+    val ds3 = ds1.map(_ + 1)
+    ds3.print()
+    env.execute("TestMap")
+  }
+}
