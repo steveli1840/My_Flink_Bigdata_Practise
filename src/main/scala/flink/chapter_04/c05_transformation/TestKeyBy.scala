b4b915371630ccf9b46170a745d568b77a3a343d
steveli1840 <steveli1840@gmail.com>
2023-07-15 23:06:54 +0800

    Test key by

---
+++ src/main/scala/flink/chapter_04/c05_transformation/TestKeyBy.scala
@@ -0,0 +1,14 @@
+package flink.chapter_04.c05_transformation
+
+import org.apache.flink.api.scala.createTypeInformation
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+
+object TestKeyBy {
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    val ds1 = env.fromCollection(List(("zhangsan", 98), ("lisi", 88), ("wangwu", 78), ("zhangsan", 68), ("lisi", 58), ("wangwu", 48)))
+    val keyedStream = ds1.keyBy(_._1)
+    keyedStream.print()
+    env.execute("TestKeyBy")
+  }
+}
